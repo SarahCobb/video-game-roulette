9e0cf76a36ebb9d6c4b60fbec63dc3f7d77e547f
Sarah Cobb <cobb.sarah@gmail.com>
2014-12-18 15:23:12 -0500

    turning debug off on live server

--- app/routes.php
+++ app/routes.php
@@ -11,77 +11,77 @@
 |
 */
 # /app/routes.php
-Route::get('/debug', function() {
+// Route::get('/debug', function() {
 
-    echo '<pre>';
+//     echo '<pre>';
 
-    echo '<h1>environment.php</h1>';
-    $path   = base_path().'/environment.php';
+//     echo '<h1>environment.php</h1>';
+//     $path   = base_path().'/environment.php';
 
-    try {
-        $contents = 'Contents: '.File::getRequire($path);
-        $exists = 'Yes';
-    }
-    catch (Exception $e) {
-        $exists = 'No. Defaulting to `production`';
-        $contents = '';
-    }
+//     try {
+//         $contents = 'Contents: '.File::getRequire($path);
+//         $exists = 'Yes';
+//     }
+//     catch (Exception $e) {
+//         $exists = 'No. Defaulting to `production`';
+//         $contents = '';
+//     }
 
-    echo "Checking for: ".$path.'<br>';
-    echo 'Exists: '.$exists.'<br>';
-    echo $contents;
-    echo '<br>';
+//     echo "Checking for: ".$path.'<br>';
+//     echo 'Exists: '.$exists.'<br>';
+//     echo $contents;
+//     echo '<br>';
 
-    echo '<h1>Environment</h1>';
-    echo App::environment().'</h1>';
+//     echo '<h1>Environment</h1>';
+//     echo App::environment().'</h1>';
 
-    echo '<h1>Debugging?</h1>';
-    if(Config::get('app.debug')) echo "Yes"; else echo "No";
+//     echo '<h1>Debugging?</h1>';
+//     if(Config::get('app.debug')) echo "Yes"; else echo "No";
 
-    echo '<h1>Database Config</h1>';
-    print_r(Config::get('database.connections.mysql'));
+//     echo '<h1>Database Config</h1>';
+//     print_r(Config::get('database.connections.mysql'));
 
-    echo '<h1>Test Database Connection</h1>';
-    try {
-        $results = DB::select('SHOW DATABASES;');
-        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-        echo "<br><br>Your Databases:<br><br>";
-        print_r($results);
-    } 
-    catch (Exception $e) {
-        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
-    }
+//     echo '<h1>Test Database Connection</h1>';
+//     try {
+//         $results = DB::select('SHOW DATABASES;');
+//         echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+//         echo "<br><br>Your Databases:<br><br>";
+//         print_r($results);
+//     } 
+//     catch (Exception $e) {
+//         echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+//     }
 
-    echo '</pre>';
+//     echo '</pre>';
 
-});
+// });
 
-Route::get('/seed', function() {
-	$tagList = array('Fighting','Maze','Arcade','Shooter','Adventure','Stealth','Horror','Graphic Adventure','Role-Playing','Japanese Role-Playing', 'Turn-Based','Simulation','Strategy','Sports','Racing','Music','Party','Puzzle','Trivia','Multiplayer','NES','SNES','Playstation','Playstation 2','Playstation 3','Playstation 4','Nintendo64','Nintendo GameCube', 'Xbox', 'Xbox 360','Xbox One','Wii', 'Wii U','Game Boy','Game Boy Color','Game Boy Advance','Nintendo DS','Nintendo 3DS', 'PC');
-	for ($i = 0; $i < count($tagList); $i++) {
-		$newTag = new Tag;
-		$newTag->name = $tagList[$i];
-		$newTag->save();
-	}
-});
+// Route::get('/seed', function() {
+// 	$tagList = array('Fighting','Maze','Arcade','Shooter','Adventure','Stealth','Horror','Graphic Adventure','Role-Playing','Japanese Role-Playing', 'Turn-Based','Simulation','Strategy','Sports','Racing','Music','Party','Puzzle','Trivia','Multiplayer','NES','SNES','Playstation','Playstation 2','Playstation 3','Playstation 4','Nintendo64','Nintendo GameCube', 'Xbox', 'Xbox 360','Xbox One','Wii', 'Wii U','Game Boy','Game Boy Color','Game Boy Advance','Nintendo DS','Nintendo 3DS', 'PC');
+// 	for ($i = 0; $i < count($tagList); $i++) {
+// 		$newTag = new Tag;
+// 		$newTag->name = $tagList[$i];
+// 		$newTag->save();
+// 	}
+// });
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+// Route::get('/', function()
+// {
+// 	return View::make('hello');
+// });
 
-Route::get('mysql-test', function() {
+// Route::get('mysql-test', function() {
 
-    # Print environment
-    echo 'Environment: '.App::environment().'<br>';
+//     # Print environment
+//     echo 'Environment: '.App::environment().'<br>';
 
-    # Use the DB component to select all the databases
-    $results = DB::select('SHOW DATABASES;');
+//     # Use the DB component to select all the databases
+//     $results = DB::select('SHOW DATABASES;');
 
-    # If the "Pre" package is not installed, you should output using print_r instead
-    var_dump($results);
+//     # If the "Pre" package is not installed, you should output using print_r instead
+//     var_dump($results);
 
-});
+// });
 
 # bind route parameters
 Route::model('game', 'Game');
